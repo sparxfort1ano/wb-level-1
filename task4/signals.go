67013bb98bb4cb0f8d880b7955d13fa9350aa7fa
Vladislav Turbin <228768837+sparxfort1ano@users.noreply.github.com>
2025-09-28 02:45:14 +0300

    task4 done

---
+++ task4/signals.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+)
+
+func worker(i int, sendCh <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for x := range sendCh {
+		fmt.Println("Воркер номер", i+1, "вывел", x)
+	}
+}
+
+func producer(rcvCh chan<- int, ctx context.Context) {
+	var x int = 1
+
+	for {
+		select {
+		case <-ctx.Done(): // ждет SIGINT
+			return
+		case <-time.After(500 * time.Millisecond): // чтобы в момент времени не выводилось на консоль очень много сообщений
+			rcvCh <- x
+			x++
+		}
+	}
+}
+
+func main() {
+	var n int
+	fmt.Print("Введите кол-во горутин-воркеров: ")
+	fmt.Scan(&n)
+
+	ch := make(chan int, n)
+
+	var wg sync.WaitGroup // программа не завершится, пока не отработают все воркеры
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker(i, ch, &wg) // n воркеров читают данные из канала
+	}
+
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt)
+	/* был выбран контекст как самый удобный handler сигналов (ctrl + C => <-ctx.Done())
+	os.Interrupt в отличие от syscall.SIGINT кроссплатформенный*/
+
+	producer(ch, ctx) // кладем данные в канал до Ctrl + C
+
+	close(ch)
+
+	wg.Wait() // ждем воркеров
+	log.Println("graceful shutdown has got made")
+}
